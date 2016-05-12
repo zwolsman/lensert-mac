5659209ad8c7aafff2db0aa66d3237c50338e832
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2016-05-12 17:02:07 +0200

    Initial Commit

---
+++ lensert-mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  lensert-mac
+//
+//  Created by Marvin on 12/05/16.
+//  Copyright © 2016 Marvin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
